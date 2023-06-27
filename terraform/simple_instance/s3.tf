c1858424427ae073d06ee3d283c768ff04566ba6
HenriWilliams <93335919+HenriWilliams@users.noreply.github.com>
2023-06-27 14:56:49 +0100

    Apply suggestions from code review
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- terraform/simple_instance/s3.tf
+++ terraform/simple_instance/s3.tf
@@ -8,6 +8,24 @@ resource "aws_s3_bucket" "dockingbay" {
   tags = {
     Name                 = "Docking Bay"
     Environment          = "Dev"
+  }
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "dockingbay" {
+  bucket = aws_s3_bucket.dockingbay.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+  bucket_prefix = "docking-bay-storage-"
+
+  tags = {
+    Name                 = "Docking Bay"
+    Environment          = "Dev"
     git_commit           = "a95498dc9df5f1dd7ff9b500bc0e1a452f32e3cd"
     git_file             = "terraform/simple_instance/s3.tf"
     git_last_modified_at = "2023-06-27 13:53:43"
